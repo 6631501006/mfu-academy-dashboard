51f10d61d2526300f1b23fdecab2c599b7d6d93e
6631501006 Kanyakrit <6631501006@lamduan.mfu.ac.th>
2026-03-02 20:37:22 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+ChartJS.register(ArcElement, Tooltip, Legend);
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const data = {
+    labels: ['Male', 'Female'],
+    datasets: [
+      {
+        label: '# of Learners',
+        data: [2800, 2307], // ตัวเลขสมมติจากดีไซน์ของคุณ 
+        backgroundColor: [
+          'rgba(142, 21, 35, 1)', // สีแดงเข้ม MFU [cite: 198, 312]
+          'rgba(189, 153, 70, 1)', // สีทอง MFU [cite: 198, 312]
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
 }
 
 export default App
